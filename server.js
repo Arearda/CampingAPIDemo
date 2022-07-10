6c3fb7027cea2bb1478b076deff9478ce9a0e534
Sarah H <73975710+Arearda@users.noreply.github.com>
2022-07-10 11:07:23 -0700

    fixing API

--- server.js
+++ server.js
@@ -6,15 +6,16 @@ try{
     const response = await fetch(`https://camping-gear-api.herokuapp.com/api/${sleepingBagName}`)
     const data = await response.json();
 
-    document.getElementById('bagName').innerText = data.bagname
-    document.getElementById('brand').innerText = data.bagbrand
-    document.getElementById('price').innerText = data.price
-    document.getElementById('temp').innerText = data.temprating
-    document.getElementById('gender').innerText = data.gender
-    document.getElementById('fill').innerText = data.fill
-    document.getElementById('weight').innerText = data.weight
-    document.getElementById('dimensions').innerText = data.dimensions
-    document.getElementById('bestUse').innerText = data.bestuse
+    document.getElementById('bagName').innerText = data.bagname[0]
+    document.getElementById('brand').innerText = data.bagbrand[0]
+    document.getElementById('price').innerText = data.price[0]
+    document.getElementById('temp').innerText = data.temprating[0]
+    document.getElementById('gender').innerText = data.gender[0]
+    document.getElementById('fill').innerText = data.fill[0]
+    document.getElementById('weight').innerText = data.weight[0]
+    document.getElementById('dimensions').innerText = data.dimensions[0]
+    document.getElementById('bestUse').innerText = data.bestuse[0]
+    console.log(data)
 
 } catch(error) {
     console.log(error)
