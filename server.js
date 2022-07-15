0bccc0383e238825d430008b4a3ecd8efec728e0
Sarah H <73975710+Arearda@users.noreply.github.com>
2022-07-15 08:36:24 -0700

    adding clear to input and bag list

--- server.js
+++ server.js
@@ -1,23 +1,65 @@
 document.querySelector('button').addEventListener('click', apiRequest);
 
-async function apiRequest() {
-const sleepingBagName = document.querySelector('input').value;
-try{
-    const response = await fetch(`https://camping-gear-api.herokuapp.com/api/${sleepingBagName}`)
-    const data = await response.json();
+//calling the apiRequest function
+async function apiRequest() { 
+    const sleepingBagName = document.querySelector('input').value;
+    let response
+    let data
 
-    document.getElementById('bagName').innerText = data[0].bagname
-    document.getElementById('brand').innerText = data[0].bagbrand
-    document.getElementById('price').innerText = data[0].price
-    document.getElementById('temp').innerText = data[0].temprating
-    document.getElementById('gender').innerText = data[0].gender
-    document.getElementById('fill').innerText = data[0].fill
-    document.getElementById('weight').innerText = data[0].weight
-    document.getElementById('dimensions').innerText = data[0].dimensions
-    document.getElementById('bestUse').innerText = data[0].bestuse
-    console.log(data)
+    try { 
+        response = await fetch(`https://camping-gear-api.herokuapp.com/api/${sleepingBagName}`)
+    }
+    catch (error) { // catch scope
+        console.log(error)
+    }
 
-} catch(error) {
-    console.log(error)
+    try {
+        data = await response.json();
+    } 
+    catch (error) {
+        console.log(error)
+    }
+
+
+document.getElementById('bagName').innerText = data[0].bagname
+document.getElementById('brand').innerText = data[0].bagbrand
+document.getElementById('price').innerText = data[0].price
+document.getElementById('temp').innerText = data[0].temprating
+document.getElementById('gender').innerText = data[0].gender
+document.getElementById('fill').innerText = data[0].fill
+document.getElementById('weight').innerText = data[0].weight
+document.getElementById('dimensions').innerText = data[0].dimensions
+document.getElementById('bestUse').innerText = data[0].bestuse
+
+
+console.log(data)
 }
-}
+
+    
+let resultsEl = document.getElementById("bag-list");
+let resetBtn = document.getElementById("clear-results");
+let clearField = document.getElementById('input-field')
+// let populateBtn = document.getElementById("populate");
+
+// let resultData = [
+//   {"name": "Filipe", "color": "Brown"},
+//   {"name": "Bruno", "color": "Red"},
+//   {"name": "Tasha", "color": "Pink"},
+//   {"name": "Sarah", "color": "Blue"},
+// ];
+
+// // When reset button is clicked, clear the results list
+resetBtn.addEventListener('click', function() {
+    resultsEl.innerHTML = "";
+    clearField.value = "";
+    console.log ("Results reset");  
+    }
+);
+
+// populateBtn.addEventListener("click", function() {
+//   resultData.forEach(function(result) {
+//      resultsEl.insertAdjacentHTML("afterbegin", "<li><b>Name:</b> " + result.name + ", " +
+//         "<b>Color:</b> " + result.color + "</li>");
+//   });
+//   console.log ("Results populated");
+// });
