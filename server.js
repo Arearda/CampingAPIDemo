d7dbae325f654331d6b3067ccb9545ecbb30c8ea
Sarah H <73975710+Arearda@users.noreply.github.com>
2022-07-22 08:10:50 -0700

    Changed CSS styles, better server code

--- server.js
+++ server.js
@@ -1,59 +1,37 @@
-document.querySelector('button').addEventListener('click', apiRequest);
-
-//calling the apiRequest function
-async function apiRequest() { 
-    const sleepingBagName = document.querySelector('input').value;
-    let response
-    let data
-
-    try { 
-        response = await fetch(`https://camping-gear-api.herokuapp.com/api/${sleepingBagName}`)
-    }
-    catch (error) { // catch scope
-        console.log(error)
-    }
-
-    try {
-        data = await response.json();
-    } 
-    catch (error) {
-        console.log(error)
-    }
-
-
-document.getElementById('bagName').innerText = data[0].bagname
-document.getElementById('brand').innerText = data[0].bagbrand
-document.getElementById('price').innerText = data[0].price
-document.getElementById('temp').innerText = data[0].temprating
-document.getElementById('gender').innerText = data[0].gender
-document.getElementById('fill').innerText = data[0].fill
-document.getElementById('weight').innerText = data[0].weight
-document.getElementById('dimensions').innerText = data[0].dimensions
-document.getElementById('bestUse').innerText = data[0].bestuse
-
-// make sure I get back data
-console.log(data)
-
+// Get a reference to elements on the page
 let resultsEl = document.getElementById("bag-list");
 let resetBtn = document.getElementById("clear-results");
-let clearField = document.getElementById('input-field')
+let clearField = document.getElementById('input-field');
 let populateBtn = document.getElementById("find-item");
+let inputField = document.querySelector('input');
+let populated = true;
+  
+populateBtn.addEventListener('click', apiRequest);
 
-// // When reset button is clicked, clear the results list
+// Clearing the input field and returned results 
 resetBtn.addEventListener('click', function() {
     resultsEl.innerHTML = "";
     clearField.value = "";
-    console.log ("Results reset");  
-    }
-);
-populateBtn.addEventListener("click", function() {
-    data.forEach(addResultToPage);
-    console.log ("Results populated");
-  });
-  
-  function addResultToPage(result) {
-       resultsEl.insertAdjacentHTML("afterbegin", "<li><Sleeping bag name:</b> " + data.bagname + ", " +
-          "<b>Brand:</b> " + data.bagbrand + "</li>");
-    }
+});
 
+//calling the apiRequest function
+async function apiRequest() { 
+    const sleepingBagName = inputField.value;
+    let response = await fetch(`https://camping-gear-api.herokuapp.com/api/${sleepingBagName}`)
+    let data = await response.json();
+    data.forEach(addResultToPage);
+}
+
+//Defining the function that renders the results of the input value to the page
+function addResultToPage(result) {
+   resultsEl.insertAdjacentHTML("afterbegin", 
+       `Sleeping Bag Name: ${result.bagname}` + '<br>' +
+       `Sleeping Bag Brand: ${result.bagbrand}` + '<br>' +
+       `Temperature Rating: ${result.temprating}` + '<br>' +
+       `Price: ${result.price}` + '<br>' +
+       `Gender: ${result.gender}` + '<br>' +
+       `Fill: ${result.fill}` + '<br>' +
+       `Weight: ${result.weight}` + '<br>' +
+       `Dimensions: ${result.dimensions}` + '<br>' +
+       `Best Use: ${result.bestuse}` + '<br>' + '<br>')
 }
